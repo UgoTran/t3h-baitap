479b1585c6edbc3ab135e56a6c0d904a9ddaf3aa
Ugo <bsk.denimit@gmail.com>
2022-05-03 21:41:26 +0700

    bai tap buoi 2, bt 3

--- src/buoi1/Runner.java
+++ src/buoi1/Runner.java
@@ -3,5 +3,9 @@ package buoi1;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
